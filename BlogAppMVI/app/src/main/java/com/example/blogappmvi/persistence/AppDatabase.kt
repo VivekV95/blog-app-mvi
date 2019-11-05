ffeccaea809e432f51a80f33d0a08813dcb40198
vivekv95 <vivekv210@gmail.com>
2019-11-04 22:56:11 -0600

    Set up Entity, Database, and Dao classes for auth

--- BlogAppMVI/app/src/main/java/com/example/blogappmvi/persistence/AppDatabase.kt
+++ BlogAppMVI/app/src/main/java/com/example/blogappmvi/persistence/AppDatabase.kt
@@ -1,5 +1,18 @@
 package com.example.blogappmvi.persistence
 
-abstract class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.blogappmvi.model.AccountPoperties
+import com.example.blogappmvi.model.AuthToken
 
+@Database(entities = [AuthToken::class, AccountPoperties::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+
+    abstract fun getAuthTokenDao(): AuthTokenDao
+
+    abstract fun getAccountPropertiesDao(): AccountPropertiesDao
+
+    companion object {
+        const val DATABASE_NAME = "app_db"
+    }
 }
