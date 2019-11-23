fac521210bd25aec84a5ffc60cf589cd27f0508c
vivekv95 <vivekv210@gmail.com>
2019-11-22 22:49:37 -0600

    Write NetworkBoundResource class to handle network responses

---
+++ BlogAppMVI/app/src/main/java/com/example/blogappmvi/repository/NetworkBoundResource.kt
@@ -0,0 +1,139 @@
+package com.example.blogappmvi.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import com.example.blogappmvi.ui.DataState
+import com.example.blogappmvi.ui.Response
+import com.example.blogappmvi.ui.ResponseType
+import com.example.blogappmvi.util.*
+import com.example.blogappmvi.util.Constants.Companion.NETWORK_TIMEOUT
+import com.example.blogappmvi.util.Constants.Companion.TESTING_NETWORK_DELAY
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+
+@InternalCoroutinesApi
+abstract class NetworkBoundResource<ResponseObject, ViewStateType>
+    (
+    isNetworkAvailable: Boolean
+) {
+
+    init {
+        setJob(initNewJob())
+        setValue(DataState.loading(true, null))
+
+        if (isNetworkAvailable) {
+            coroutineScope.launch {
+                delay(TESTING_NETWORK_DELAY)
+
+                withContext(Main) {
+                    val apiResponse = createCall()
+                    result.addSource(apiResponse) { response ->
+                        result.removeSource(apiResponse)
+
+                        coroutineScope.launch {
+                            handleNetworkCall(response)
+                        }
+                    }
+                }
+            }
+
+            GlobalScope.launch(IO) {
+                delay(NETWORK_TIMEOUT)
+            }
+
+            if (!job.isCompleted) {
+                job.cancel(CancellationException((UNABLE_TO_RESOLVE_HOST)))
+            }
+        } else {
+            onErrorReturn(UNABLE_TODO_OPERATION_WO_INTERNET, true, shouldUseToast = false)
+        }
+    }
+
+    private suspend fun handleNetworkCall(response: GenericApiResponse<ResponseObject>?) {
+        when (response) {
+            is ApiSuccessResponse -> {
+                handleApiSuccessResponse(response)
+            }
+            is ApiErrorResponse -> {
+                onErrorReturn(response.errorMessage, shouldUseDialog = true, shouldUseToast = false)
+            }
+            is ApiEmptyResponse -> {
+                onErrorReturn("HTTP 204. Returned Nothing", shouldUseDialog = true, shouldUseToast = false)
+            }
+        }
+    }
+
+    protected val result = MediatorLiveData<DataState<ViewStateType>>()
+    protected lateinit var job: CompletableJob
+    protected lateinit var coroutineScope: CoroutineScope
+
+    fun onCompleteJob(dataState: DataState<ViewStateType>) {
+        GlobalScope.launch(Main) {
+            job.complete()
+            setValue(dataState)
+        }
+    }
+
+    private fun setValue(dataState: DataState<ViewStateType>) {
+        result.value = dataState
+    }
+
+
+    fun onErrorReturn(errorMessage: String?, shouldUseDialog: Boolean, shouldUseToast: Boolean) {
+        var msg = errorMessage
+        var useDialog = shouldUseDialog
+        var responseType: ResponseType = ResponseType.None()
+        if (msg == null) {
+            msg = ERROR_UNKNOWN
+        } else if (isNetworkError(msg)) {
+            msg = ERROR_CHECK_NETWORK_CONNECTION
+            useDialog = false
+        }
+        if (shouldUseToast) {
+            responseType = ResponseType.Toast()
+        }
+        if (useDialog) {
+            responseType = ResponseType.Dialog()
+        }
+        onCompleteJob(
+            DataState.error(
+                response = Response(
+                    message = msg,
+                    responseType = responseType
+                )
+            )
+        )
+    }
+
+    @InternalCoroutinesApi
+    private fun initNewJob(): Job {
+        job = Job()
+        job.invokeOnCompletion(
+            onCancelling = true,
+            invokeImmediately = true,
+            handler = object : CompletionHandler {
+                override fun invoke(cause: Throwable?) {
+                    if (job.isCancelled) {
+                        cause?.let {
+                            onErrorReturn(
+                                it.message,
+                                shouldUseDialog = false,
+                                shouldUseToast = true
+                            )
+                        } ?: onErrorReturn(ERROR_UNKNOWN, false, true)
+                    }
+                }
+            })
+        coroutineScope = CoroutineScope(Dispatchers.IO + job)
+        return job
+    }
+
+    fun asLiveData() = result as LiveData<DataState<ViewStateType>>
+
+    abstract suspend fun handleApiSuccessResponse(response: ApiSuccessResponse<ResponseObject>)
+
+    abstract fun createCall(): LiveData<GenericApiResponse<ResponseObject>>
+
+    abstract fun setJob(job: Job)
+}
