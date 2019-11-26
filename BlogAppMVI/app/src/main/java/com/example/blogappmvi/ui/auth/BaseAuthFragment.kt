61b550153b8c3367e0c9daeaeab8e4f561c4d12e
vivekv95 <vivekv210@gmail.com>
2019-11-26 01:07:52 -0600

    Use WebView to reset user password

--- BlogAppMVI/app/src/main/java/com/example/blogappmvi/ui/auth/BaseAuthFragment.kt
+++ BlogAppMVI/app/src/main/java/com/example/blogappmvi/ui/auth/BaseAuthFragment.kt
@@ -1,16 +1,21 @@
 package com.example.blogappmvi.ui.auth
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import com.example.blogappmvi.ui.DataStateChangedListener
 import com.example.blogappmvi.viewmodel.ViewModelProviderFactory
 import dagger.android.support.DaggerFragment
+import java.lang.ClassCastException
 import java.lang.Exception
 import javax.inject.Inject
 
 abstract class BaseAuthFragment: DaggerFragment() {
 
     val TAG = "AppDebug"
+
+    lateinit var stateChangeListener: DataStateChangedListener
 
     @Inject
     lateinit var viewModelProviderFactory: ViewModelProviderFactory
@@ -23,5 +28,14 @@ abstract class BaseAuthFragment: DaggerFragment() {
         viewModel = activity?.run {
             ViewModelProvider(this, viewModelProviderFactory).get(AuthViewModel::class.java)
         }?: throw Exception("Plssssssss")
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try {
+            stateChangeListener = context as DataStateChangedListener
+        } catch (e: ClassCastException) {
+
+        }
     }
 }
